0f2f8e898fc1232cd49d1dc08b3c9857907fd6cd
Joseph_Tully <tully4school@gmail.com>
2020-02-15 04:01:11 -0500

    created Get functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import schema from './schema'
+import resolvers from './resolvers'
 
 const app = express()
 
@@ -8,37 +9,7 @@ app.get('/', (req, res) => {
   res.send('GraphQL is Amazing!')
 })
 
-class Friend {
-  constructor (id, { firstName, lastName, gender, language, email }) {
-    this.id = id
-    this.firstName = firstName
-    this.lastName = lastName
-    this.gender = gender
-    this.language = language
-    this.email = email
-  }
-}
-
-const friendDatabase = {}
-
-const root = {
-  friend: () => {
-    return {
-      id: 234235,
-      firstName: 'Joe',
-      lastName: 'Tully',
-      gender: 'Male',
-      email: [{ email: 'jtully84@gmail.com' }, { email: 'tully_j@hotmail.com' }]
-    }
-  },
-  createFriend: ({ input }) => {
-    let id = require('crypto')
-      .randomBytes(10)
-      .toString('hex')
-    friendDatabase[id] = input
-    return new Friend(id, input)
-  }
-}
+const root = resolvers
 
 app.use(
   '/graphql',
