af4ad092af86b374f2d30d17f31e6dc849136184
Joseph_Tully <tully4school@gmail.com>
2020-02-15 03:14:44 -0500

    created fake data to maniplate in the graphql query editor

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import schema from './schema'
 
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('GraphQL is Amazing!')
 })
+
+const root = {
+  friend: () => {
+    return {
+      id: 234235,
+      firstName: 'Joe',
+      lastName: 'Tully',
+      gender: 'Male',
+      emails: [
+        { email: 'jtully84@gmail.com' },
+        { email: 'tully_j@hotmail.com' }
+      ]
+    }
+  }
+}
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+)
 
 app.listen(8080, () =>
   console.log('running the server on localhost:8080/graphql')
